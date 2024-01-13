a94270056b265ca79b13ebfdb8239783a06b8f8b
Kaine Binch <kainebinch@live.com>
2024-01-13 16:54:52 +0000

    feat: added ring to avatar

--- src/components/myFace.jsx
+++ src/components/myFace.jsx
@@ -3,7 +3,7 @@ import headImage from "../assets/kaineHead.jpg";
 const MyFace = ({ size = 140 }) => {
   return (
     <div className="avatar">
-      <div className="rounded-full" style={{ width: size }}>
+      <div className="rounded-full ring ring-neutral ring-offset-neutral-100 ring-offset-2 ring-opacity-20" style={{ width: size }}>
         <img src={headImage} />
       </div>
     </div>

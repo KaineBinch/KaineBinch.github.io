1a3f0c377fe14d2a861036f30fecbef2d4f0f234
Kaine Binch <kainebinch@live.com>
2024-01-22 15:01:05 +0000

    chore: updated text

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -41,36 +41,30 @@ const HomePage = () => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6, delay: 1.8 }}
               >
-                Nottingham based software engineer... Lorem ipsum dolor sit,
-                amet consectetur adipisicing elit. Repellat, quam eius molestias
-                eveniet nobis provident animi pariatur, quo, repudiandae
-                inventore ipsum deserunt. Pariatur totam iusto laborum hic
-                excepturi laboriosam? Veniam.
+                Nottingham based software engineer dedicated to transforming ideas into efficient, user-centric solutions. With a fresh perspective and a passion for innovation, my portfolio showcases projects that reflect my commitment to excellence in coding. 
               </m.p>
             </div>
           </div>
         </div>
         <hr className="border-neutral-300" />
         <div className="max-w-full flex bg-neutral md:flex-row flex-col p-10">
-          <div className="flex-1">
+          <div className="flex-1 text-neutral-200">
             <h1 className="text-5xl font-bold mb-5">About me</h1>
             <a
-              className="btn btn-primary hidden md:inline-flex"
+              className="btn btn-primary text-neutral-200 hidden md:inline-flex"
               href="/#/about"
             >
               Learn more
             </a>
           </div>
-          <div className="flex flex-col flex-[2] px-2 text-start ">
+          <div className="flex flex-col flex-[2] px-2 text-start text-neutral-200">
             <p>
-              I am currently focusing on javascript. This website has been built
-              with react, and I have also used a sprinkling of tailwind css and
-              daisyUI.
+              Right now I am focusing on mastering javascript. This website has been built
+              with react, I have also used tailwind css, framer motion and
+              daisyUI for various elements. However, I do aspire to broaden my skills by learning a diverse range of programming languages.
             </p>
             <p>
-              So, why the shift to software engineering? In recent years, i{"'"}
-              ve been moving into more technical roles, and it has been evident
-              that this is where my skills are best suited.
+              I currently work in the social care sector as a team manager, but in recent years, i{"'"}ve been moving into more technical roles, and it has been evident that this is where my skills are best suited.
             </p>
             <a
               className="btn btn-primary md:hidden mt-5 self-center"

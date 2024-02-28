343d4c0602492117dafac59a76673c47aec28cd0
KaineBinch <kainebinch@live.com>
2024-02-28 10:35:01 +0000

    feat: installed fontawesome

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { HashRouter } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  fab,
+  faSquareFacebook,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faChevronRight,
+  faPhone,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  fab,
+  faChevronRight,
+  faPhone,
+  faEnvelope,
+  faSquareFacebook,
+  faInstagram,
+  faLinkedin
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

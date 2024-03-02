23ae2d05c5e96599b2a77c497b888d29a9a1ce8b
KaineBinch <kainebinch@live.com>
2024-03-02 13:46:35 +0000

    feat: cv header for web complete

--- src/components/cv/cvHeader.jsx
+++ src/components/cv/cvHeader.jsx
@@ -1,13 +1,29 @@
-import image from "../assets/cvHeadPicture.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import image from "../../assets/cvHeadPicture.jpg";
+import cvPdf from "../../assets/KaineCV.pdf";
 
-const cvHeader = () => {
+const CvHeader = () => {
   return (
     <>
-      <div className="flex">
-        <img src={image} />
+      <div className="flex place-content-between md:mt-32 h-[310px] w-screen mt-24 bg-white max-w-[755px] max-h-[1067px]">
+        <div className="flex flex-row">
+          <img className="md:h-[300px] md:w-[300px]" src={image} />
+          <div className="flex flex-col w-[310px] md:h-64 justify-center items-start text-[#1c1a73]">
+            <div className="text-6xl font-extrabold">KAINE</div>
+            <div className="pl-[50px] pt-2 text-6xl font-extrabold">BINCH</div>
+            <div className="pl-[100px] pt-2 text-xl">Software Engineer</div>
+          </div>
+        </div>
+        <a href={cvPdf} className="h-[85px] w-[85px] ">
+          <FontAwesomeIcon
+            className="mt-10 mr-10 w-[40px] h-[40px]"
+            icon="fa-solid fa-file-pdf"
+            style={{ color: "#1c1a73" }}
+          />
+        </a>
       </div>
     </>
   );
 };
 
-export default cvHeader;
+export default CvHeader;

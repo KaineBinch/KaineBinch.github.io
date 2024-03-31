98b4c33a28f3ee016df1479711bb7ad53866f9f3
KaineBinch <kainebinch@live.com>
2024-03-31 03:22:54 +0100

    feat: projects updated

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,15 +1,15 @@
 const ProjectCard = ({ title, shortDesc, longDesc, tags, link, iconImage }) => {
   return (
     <div>
-      <div className="card bg-base-300 bg-opacity-10 backdrop-blur-md shadow-xl">
-        <figure>
+      <div className="card bg-base-300 bg-opacity-10 backdrop-blur-md shadow-xl ">
+        <div className="w-400px h-150px min-w-[300px] min-h-[150px]">
           <img src={iconImage} />
-        </figure>
-        <div className="card-body">
+        </div>
+        <div className="card-body h-[300px]">
           <h2 className="card-title">{title}</h2>
           <p className="text-start visible xl:hidden">{shortDesc}</p>
           <p className="text-start xl:block hidden">{longDesc}</p>
-          <div className="card-actions justify-end mb-2">
+          <div className="card-actions justify-end mb-5">
             <a
               href={link}
               rel="noreferrer"
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,5 +1,7 @@
 import DashboardImage from "../assets/projectAssets/dashboard.png"
 import DashboardIcon from "../assets/projectAssets/smallDashboard.png";
+import passGen from "../assets/projectAssets/passwordGenerator.png"
+import pixelArt from "../assets/projectAssets/pixelArt.png"
 
 export const projects = [
   {
@@ -11,4 +13,23 @@ export const projects = [
     tags: ["react", "tailwind", "data"],
     link: "https://kainebinch.dev/adminDashboard",
   },
+  {
+    title: "Password Generator",
+    hoverImage: "",
+    iconImage: passGen,
+    shortDesc: "A simple password generator along with a copy function for easy to use password creation.",
+    longDesc: "A simple and straightforward password generator with a view to simplify the password generation process with unique and secure passwords. This project also includes a copy function to further ease the process.",
+    tags: ["HTML", "Javascript", "CSS"],
+    link: "https://kainebinch.dev/PasswordGenerator"
+  },
+  {
+    title: "Pixel Art Generator",
+    hoverImage: "",
+    iconImage: pixelArt,
+    shortDesc: "A grid generator with a color wheel that allows you to paint and erase each element within the grid",
+    longDesc: "A modifiable grid generator featuring a color wheel for painting and erasing grid elements. This allows you to paint and erase elements, creating unique designs.",
+    tags: ["HTML", "Javascript", "CSS"],
+    link: "https://kainebinch.dev/Grid-Painter"
+  },
+
 ]
